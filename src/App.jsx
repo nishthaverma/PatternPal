9a64df06eae241d1b2f9cebe8b1d49e9207358b8
nishthaverma <nishthaverma0204@gmail.com>
2024-09-15 21:04:57 -0500

    current

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ export default function App() {
             </Flex>
           </View>
           <Divider />
-          <Heading level={2}>Current Notes</Heading>
+          <Heading level={2}>Current Patterns</Heading>
           <Grid
             margin="3rem 0"
             autoFlow="column"
